354a37c3b7726455fab5f2b63c42f2eda79ae0b5
Gülsah <73911627+GulsahDurmaz@users.noreply.github.com>
2022-11-19 10:29:12 +0100

    update

--- uejbej.py
+++ uejbej.py
@@ -1,10 +1,10 @@
 n = int(input('Write a number'))
-if n%3 == 0 and n%5 != 0:
+if n%15 == 0:
+      print('üjbej')
+elif  n%3 == 0 and n%5 != 0:
       print('3')
 elif n%5 == 0 and n%3 != 0:
       print('5')
-elif n%15 == 0:
-      print('üjbej')
 else: 
       quit
 
