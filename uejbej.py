209e014131f0a18a95d456e1bf17426261d5c2cf
Gülsah <73911627+GulsahDurmaz@users.noreply.github.com>
2022-11-19 09:52:21 +0100

    first codes

---
+++ uejbej.py
@@ -0,0 +1,10 @@
+n = int(input('Write a number'))
+if n%3 == 0 and n%5 != 0:
+    Print('3')
+elif n%5 == 0 and n%3 != 0:
+      Print('5')
+elif n%15 == 0:
+      Print('üjbej')
+else: 
+    Print('non')
+
